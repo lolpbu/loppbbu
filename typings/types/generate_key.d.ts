5276b13112d08a1e11a1359261088eb7abe7b4dd
lolpbu <viniciusidalino@hotmail.com>
2020-09-11 23:46:23 -0300

    added a description to each function

--- typings/types/generate_key.d.ts
+++ typings/types/generate_key.d.ts
@@ -1,3 +1,17 @@
+/**
+ * returns a random string of uppercase and lowercase letters and numbers
+ * @param l the string's length
+ */
 export declare function generateKey(l: number): string;
+
+/**
+ * returns a random string of uppercase and lowercase letters
+ * @param l the string's length
+ */
 export declare function generateLetters(l: number): string;
+
+/**
+ * returns a random string of numbers
+ * @param l the string's length
+ */
 export declare function generateNumbers(l: number): string;
